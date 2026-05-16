25fde5f6364c3aa3733c58bfb5c59f3246b6b83c
Kaio Souza <kaio.souza3020@gmail.com>
2026-05-15 20:58:38 -0400

    Mega atualização em todos os arquivos

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -9,7 +9,7 @@ const categories = ['Todos', 'Rodas', 'Eventos', 'Histórico'];
 const galleryItems = [
   { id: 1, category: 'Eventos', title: 'Batizado 2025', image: '/galeria/roda1.jpg' },
   { id: 2, category: 'Rodas', title: 'Roda na Eduardo Ribeiro', image: '/galeria/treino1.jpg' },
-  { id: 3, category: 'Histórico', title: 'Mestre Camisa e Mestre Canário (27 Anos ACBP)', image: 'public/membros/mestre_camisa_e_mestre_canário.jpeg' },
+  { id: 3, category: 'Histórico', title: 'Mestre Camisa e Mestre Canário (27 Anos ACBP)', image: './membros/mestre_camisa_e_mestre_canário.jpeg' },
 ];
 
 // OS SEUS LINKS DO GOOGLE FOTOS ORGANIZADOS
